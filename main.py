04177b99dd1987ff8810bc2c9c364713d496ed6e
YassineGharabi <gharabiyassine18@gmail.com>
2025-09-26 15:46:13 +0100

    Ajouter affichage ALICE DUPONT (25 ans) aime : 1984, Le petit Prince

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from data import utilisateurs , aime_livres
-from users import utilisateur_majeur , utilisateur_livres , nom_complete_majuscules
+from users import utilisateur_majeur , to_string , utilisateur_livres , nom_complete_majuscules
 
 
 # tester fonction utilisateur_majeur qui filter par age
-# print(utilisateur_majeur(utilisateurs))
+print(utilisateur_majeur(utilisateurs))
 
 # tester fonction Formater les noms complets en majuscules
-# print(nom_complete_majuscules(utilisateurs))
+print(nom_complete_majuscules(utilisateurs))
 
 # tester fonction Créer un dictionnaire associant chaque utilisateur à ses livres aimés
 print(utilisateur_livres(utilisateurs,aime_livres))
+
+
+# tester fonction to string Générer un résumé par utilisateur 
+user_fav_books = utilisateur_livres(utilisateurs,aime_livres)
+to_string(user_fav_books)
+
--- users.py
+++ users.py
@@ -18,7 +18,16 @@ def utilisateur_livres(users,books):
         for book in books:
             if user[0] == book[0]:
                 fav_books.append(book[1])
-
         return dict( name = f"{user[1]} {user[2]}".upper() , age = user[3] , fav_books = fav_books )
 
     return list(map(fun_logique,users))
+
+
+def to_string(users_fav_books):
+    
+    for user in users_fav_books:
+        books = ""
+        for book in user["fav_books"]:
+            if len(book) != 0:
+                books += " , " + book
+        print(f"{user["name"]} ({user["age"]} ans) {books}")
